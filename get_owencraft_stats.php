570ebd0d9b0a6d60e8b5aec7d28755aebd74d750
Benjamin Owen <accounts@projecttiy.com>
2022-04-21 18:48:43 -0400

    added in a prom class to deal with prometheus dependencies, testing out class variables with var_dump

--- get_owencraft_stats.php
+++ get_owencraft_stats.php
@@ -3,10 +3,17 @@
     require_once "logging.class.php";
     $logger = new oclogger("/var/log/", "owencraft_stats.log");
 
+    require_once "prom.class.php";
+    $prom = new ocprom("/var/prometheus/", "owencraft_stats.prom", "/tmp/prometheus/", "owencraft_stats-tmp.prom");
+
+    require_once "owencraft.class.php";
+    $oc = new owencraft();
+
     $logger->startScript();
 
-    $msg = "Testing logging class...";
-    $logger->logMsg($msg, 0);
+    var_dump($logger);
+    var_dump($prom);
+    var_dump($oc);
 
     $logger->stopScript();
 
--- owencraft.class.php
+++ owencraft.class.php
@@ -1,5 +1,15 @@
 <?php
 
+    class owencraft {
 
+        public $objectives = array();
+
+        function __construct() {
+
+            $this->objectives = array("minecraft:crafted", "minecraft:mined", "minecraft:custom", "minecraft:dropped", "minecraft:used", "minecraft:broken", "minecraft:killed");
+
+        }
+
+    }
 
 ?>
